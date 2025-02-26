41e57d157f4bd5d30a134a595c643c3b01772c84
Akande Lateef <akandelateef0@gmail.com>
2025-02-26 22:04:39 +0100

    add:login and registration joi validation added

---
+++ backend/authentication/validation/authValidation.js
@@ -0,0 +1,50 @@
+const Joi = require('joi')
+
+const registerSchema = Joi.object({
+    firstname: Joi.string()
+            .min(2).max(30)
+            .required()
+            .messages({
+      "any.required": "Firstname is required",
+    }),
+    lastname: Joi.string()
+            .min(2).max(30)
+            .required()
+            .messages({
+      "any.required": "Lastname is required",
+    }),
+    email: Joi.string()
+            .email()
+            .required()
+            .messages({
+      "string.email": "Invalid email format",
+      "any.required": "Email is required",
+    }),
+
+    password: Joi.string()
+                .min(8)
+                .required()
+                .messages({
+                    "string.min": "Password must be at least 8 characters",
+                    "any.required": "Password is required",
+    }),
+    repeat_password: Joi.ref('password'),
+  });
+
+  const loginSchema = Joi.object({
+    email: Joi.string()
+            .email()
+            .required()
+            .messages({
+      "string.email": "Invalid email format",
+      "any.required": "Email is required",
+    }),
+
+    password: Joi.string()
+                .required()
+                .messages({
+                    "any.required": "Password is required",
+    }),
+  });
+
+  module.exports = { registerSchema, loginSchema };
